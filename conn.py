a0ae8731536994bf23530aa029a9e67e64f5a857
Diro35 <ryan.mzalouat@epitech.eu>
2024-10-10 12:09:44 +0200

    Javascript importe données comme nom et prenom

--- conn.py
+++ conn.py
@@ -1,8 +1,8 @@
-import pymysql
+import pymysql #Librairie auxliaire
 conn = pymysql.connect(host="localhost",user='root',database="Database")
 arp = conn.cursor()
 arp.execute('SELECT * FROM Annonce')
 result = arp.fetchall()
 for row in result: 
-    print(row) 
+    print(type(row)) 
     print("\n") 
---
+++ user_form.js
@@ -0,0 +1,12 @@
+function SetUtilisateur() {
+    nom= document.getElementById("nom").value
+    prenon= document.getElementById("prenom").value
+    email= document.getElementById("email").value
+    telephone= document.getElementById("telephone").value
+    mdp= document.getElementById("mdp").value
+    //boite.onclick = function () 
+    //{
+        console.log(nom)
+    //}
+}
+//clic()
