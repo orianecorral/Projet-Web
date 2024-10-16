453839664d71ccf8d7cd94e088a52e2167895d21
Diro35 <ryan.mzalouat@epitech.eu>
2024-10-16 15:05:21 +0200

    tableau admin

--- jobfinder/jobfinder_admin/urls.py
+++ jobfinder/jobfinder_admin/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [ 
-    path('administration/', administration, name='administration'),
+    path('tab_Particulier/', tab_Particulier, name='tab_Particulier'),
+    path('tab_entreprise/', tab_entreprise, name='tab_entreprise'),
+    path('tab_annonce/', tab_annonce, name='tab_annonce'),
 ] 
--- jobfinder/jobfinder_admin/views.py
+++ jobfinder/jobfinder_admin/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.apps import apps
 from jobfinder_compte.models import Particuliers
+from jobfinder_compte.models import Entreprises
+from jobfinder_app.models import Annonces
 # Create your views here.
-def administration(request):
-    print('test')
+def tab_Particulier(request):
     Particulier =Particuliers.objects.all()
-    return render(request,'Liste_utilisateur.html',{'groupe':Particulier})
+    return render(request,'Liste_utilisateur.html',{'particulier':Particulier})
+
+def tab_entreprise(request):
+    Entreprise =Entreprises.objects.all()
+    return render(request,'Liste_utilisateur.html',{'entreprises':Entreprise})
+def tab_annonce(request):
+    annonces =Annonces.objects.all()
+    return render(request,'Liste_utilisateur.html',{'annonces':annonces})
