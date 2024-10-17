14ef5b9d47ebc59364b21488172bdfba08be2104
Diro35 <ryan.mzalouat@epitech.eu>
2024-10-17 12:03:54 +0200

    avancement page admin

--- jobfinder/jobfinder_admin/forms.py
+++ jobfinder/jobfinder_admin/forms.py
@@ -1,2 +1,11 @@
 # myapp/forms.py 
 from django import forms 
+from jobfinder_compte.models import Particuliers
+from jobfinder_compte.models import Entreprises
+from jobfinder_app.models import Annonces
+from .forms import *
+
+class particulierForm(forms.ModelForm):
+    class Meta:
+        model = Particuliers
+        fields = '__all__'
--- jobfinder/jobfinder_admin/views.py
+++ jobfinder/jobfinder_admin/views.py
@@ -3,10 +3,16 @@ from django.apps import apps
 from jobfinder_compte.models import Particuliers
 from jobfinder_compte.models import Entreprises
 from jobfinder_app.models import Annonces
+from .forms import *
+from django.contrib.auth import authenticate
 # Create your views here.
 def tab_Particulier(request):
+    #context = {}
+    particulierform = particulierForm()
     Particulier =Particuliers.objects.all()
-    return render(request,'Liste_utilisateur.html',{'particulier':Particulier})
+    #context['Particulier'] = Particulier
+    #context['form'] = particulierform
+    return render(request,'Liste_utilisateur.html',{'particulier':Particulier,'part':particulierform},)
 
 def tab_entreprise(request):
     Entreprise =Entreprises.objects.all()
@@ -14,3 +20,12 @@ def tab_entreprise(request):
 def tab_annonce(request):
     annonces =Annonces.objects.all()
     return render(request,'Liste_utilisateur.html',{'annonces':annonces})
+def CoAdmin(request):
+    print('testtttttttt')
+    user = authenticate(is_superuser = 1)
+    if user is not None:
+        print('je suis co')
+        return render(request,'Liste_utilisateur.html')
+    else:
+        print('jesuis pasco')
+        return render(request,'Liste_utilisateur.html')
